58de70b09f9796ce53165152f0ac77a786b0ad8b
maciek96 <mmika1496@gmail.com>
2017-12-28 22:01:53 +0100

    usuniety uml inv-ser

--- invoices-server/src/main/java/com/io/invoices/invoiceshibernate/product/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.io.invoices.invoiceshibernate.product;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Integer>{
-}
