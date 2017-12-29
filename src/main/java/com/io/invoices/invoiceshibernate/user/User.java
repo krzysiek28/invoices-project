12ee41d9c04ec0bfe598d7390f18aab2eb93f592
maciek96 <mmika1496@gmail.com>
2017-12-29 14:10:36 +0100

    dodany name do usera i sie kompiluje

--- src/main/java/com/io/invoices/invoiceshibernate/user/User.java
+++ src/main/java/com/io/invoices/invoiceshibernate/user/User.java
@@ -17,15 +17,25 @@ public class User {
     @Id
     @GeneratedValue
     private Integer id;
+    private String name;
     private String email;
     private String password;
 
-    public User(String email, String password) {
+    public User(String name, String email, String password) {
+        this.name = name;
         this.email = email;
         this.password = password;
     }
 
     public User(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getEmial() {
         return email;
