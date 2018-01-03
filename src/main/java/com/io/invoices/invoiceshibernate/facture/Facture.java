2d25de829927193a398423136089a3d8916185b0
maciek96 <mmika1496@gmail.com>
2018-01-03 12:37:06 +0100

    usuniety client

--- src/main/java/com/io/invoices/invoiceshibernate/facture/Facture.java
+++ src/main/java/com/io/invoices/invoiceshibernate/facture/Facture.java
@@ -1,10 +1,10 @@
 package com.io.invoices.invoiceshibernate.facture;
 
-import com.io.invoices.invoiceshibernate.client.Client;
 import com.io.invoices.invoiceshibernate.firm.Firm;
 import com.io.invoices.invoiceshibernate.user.User;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Facture {
@@ -16,18 +16,18 @@ public class Facture {
     private User user;
     @OneToOne
     private Firm userFirm;
-    @OneToOne
-    private Client client;
+    @OneToMany
+    private List<Firm> clients;
     private String name;
     private String place;
     private Float price;
 
     public Facture(){}
 
-    public Facture(User user, Firm userFirm, Client client, String name, String place, Float price) {
+    public Facture(User user, Firm userFirm, List<Firm> clients, String name, String place, Float price) {
         this.user = user;
         this.userFirm = userFirm;
-        this.client = client;
+        this.clients = clients;
         this.name = name;
         this.place = place;
         this.price = price;
@@ -57,12 +57,12 @@ public class Facture {
         this.userFirm = userFirm;
     }
 
-    public Client getClient() {
-        return client;
+    public List<Firm>  getClients() {
+        return clients;
     }
 
-    public void setClient(Client client) {
-        this.client = client;
+    public void setClients(List<Firm>  client) {
+        this.clients = clients;
     }
 
     public String getName() {
