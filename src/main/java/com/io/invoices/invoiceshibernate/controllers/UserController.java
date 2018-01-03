44874e2e7672f786d4f3877ff759bb16130fb71e
aolesek <arkadiusz.olesek@gmail.com>
2018-01-03 12:44:13 +0100

    User Controller, User Service, User Repository

--- src/main/java/com/io/invoices/invoiceshibernate/controllers/UserController.java
+++ src/main/java/com/io/invoices/invoiceshibernate/controllers/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/users")
 public class UserController {
     @Autowired
     private UserService userService;
