ee6eeb06605fa5e007cbe723ff48cb52c59f46ab
maciek96 <mmika1496@gmail.com>
2018-01-04 19:58:20 +0100

    trying to send pdf

--- src/main/java/com/io/invoices/invoiceshibernate/controllers/FactureController.java
+++ src/main/java/com/io/invoices/invoiceshibernate/controllers/FactureController.java
@@ -5,9 +5,14 @@ import com.io.invoices.invoiceshibernate.facture.FactureService;
 import com.io.invoices.invoiceshibernate.pdfCreator.PdfCreator;
 import org.dom4j.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.List;
 
 @RestController
@@ -29,7 +34,21 @@ public class FactureController {
 
         PdfCreator pdfCreator = new PdfCreator(getFacture(id));
         pdfCreator.createPdf();
+    }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/send")
+    public ResponseEntity<byte[]> getPDF(@RequestBody String json) throws IOException {
+        File initialFile = new File("src/main/resources/sample.txt");
+        InputStream input = new FileInputStream(initialFile);
+        byte[] contents = org.apache.commons.io.IOUtils.toByteArray(input);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("application/pdf"));
+        String filename = "output.pdf";
+        headers.setContentDispositionFormData(filename, filename);
+        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(contents, headers, HttpStatus.OK);
+        return response;
     }
 /*    @RequestMapping(method = RequestMethod.POST, value = "/{ownerId}/factures")
     public void addFacture(@PathVariable String ownerId, @RequestBody Facture facture) {
