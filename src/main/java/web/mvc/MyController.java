e17fe45953b04cc861fb157a9c7ac2ad8c7e2791
maciek96 <mmika1496@gmail.com>
2018-01-09 14:08:57 +0100

    user after login have to choose firm

--- src/main/java/web/mvc/MyController.java
+++ src/main/java/web/mvc/MyController.java
@@ -57,22 +57,8 @@ public class MyController {
         status.setComplete();
         return "loginPage";
     }
-    @RequestMapping(value = "/chooseFirm")
-    public String chooseFirm(ModelMap modelMap) throws URISyntaxException, JSONException, IOException {
-        modelMap.addAttribute("authservice", userAuthenticationService);
-        if (userAuthenticationService.isLoggedIn()) {
-            userService.setUserId();
-            modelMap.addAttribute("firms", firmService.getFirms());
-        }
-        return "chooseFirm";
-    }
 
-    @RequestMapping(value = "/chooseFirm/{id}")
-    public String setFirmId(@PathVariable("id") String id){
-        userAuthenticationService.setFirmId(Integer.parseInt(id));
 
-        return "homeLogged";
-    }
 
     @RequestMapping(value = "/homeLogged")
     public String homeLoged(ModelMap model) throws URISyntaxException, JSONException, IOException {
@@ -138,29 +124,7 @@ public class MyController {
         return "redirect:/homeLogged";
     }
 
-    @RequestMapping(value = "/firms")
-    public String menageFirm(HttpServletRequest request,
-                             ModelMap modelMap) {
-        try {
-            modelMap.addAttribute("firms", firmService.getFirms());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "firms";
-    }
 
-    @RequestMapping(value = "/firms/addfirm", method = RequestMethod.POST)
-    public String addFirm(@RequestParam("email") String email,
-                          @RequestParam("name") String name,
-                          @RequestParam("nip") String nip,
-                          @RequestParam("phone") String phone,
-                          @RequestParam("place") String place,
-                          SessionStatus status) throws URISyntaxException, JSONException {
-        firmService.addFirm(email, name, nip, phone, place);
-        status.setComplete();
-
-        return "redirect:/firms";
-    }
 
     @RequestMapping(value = "/products")
     public String productsPage(HttpServletRequest request,
