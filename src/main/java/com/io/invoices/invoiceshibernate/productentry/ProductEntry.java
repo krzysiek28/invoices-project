c946a40ddcfe3a85142c22a8bd5075cb8b0c7b64
aolesek <arkadiusz.olesek@gmail.com>
2018-01-11 11:35:09 +0100

    toPay and total field in facture, no field in productentry

--- src/main/java/com/io/invoices/invoiceshibernate/productentry/ProductEntry.java
+++ src/main/java/com/io/invoices/invoiceshibernate/productentry/ProductEntry.java
@@ -14,7 +14,23 @@ public class ProductEntry {
     private Integer id;
     @OneToOne
     private Product product;
+
+    public ProductEntry(Product product, Float quantity, int no) {
+        this.product = product;
+        this.quantity = quantity;
+        this.no = no;
+    }
+
     private Float quantity;
+    private int no;
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
 
     public ProductEntry() {
 
